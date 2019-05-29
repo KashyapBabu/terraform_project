7750f03e01d934ddcdfabf8c3007d9bdd2defdde
kashyapbabu <kashyapbabu1111@gmail.com>
2019-05-29 15:20:57 -0600

    Script not fully functional: Just an update to store file

--- com.tf
+++ com.tf
@@ -1,3 +1,28 @@
+#Creating a VPC
+resource "google_compute_network" "vpc" {
+   name                  = "${var.name}-vpc"
+   auto_create_subnetworks = "false"
+}
+
+#Creating a subnet
+resource "google_compute_subnetwork" "subnet" {
+   name = "${var.name}-subnet"
+   ip_cidr_range = "${var.subnet_cidr}"
+   network       = "${var.name}-vpc"
+   depends_on = [google_compute_network.vpc.name]
+
+   allow {
+       protocol = "icmp"
+   }
+
+   allow {
+       protocol = "tcp"
+       ports    = ["22"]
+   }
+
+   source_ranges = ["0.0.0.0/0"] 
+}
+
 #create a new instance
 resource "google_compute_instance" "default" {
    name = "myvm"
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "region" {}
+variable "gcp_project" {}
+varibale "credentials" {}
+variable "name" {}
+variable "subnet_cidr" {}
